c4eef5f0863189226ae11090fdd53b92e608d4c6
Desm00nt <deniska.kartuzov@gmail.com>
2026-09-09 18:34:53 +0300

    Add top-down prototype camera

---
+++ src/main/java/com/desm00nt/machinedefence/MachineClient.java
@@ -0,0 +1,23 @@
+package com.desm00nt.machinedefence;
+
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ViewportEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = MachineDefence.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public final class MachineClient {
+    private MachineClient() {
+    }
+
+    @SubscribeEvent
+    public static void camera(ViewportEvent.ComputeCameraAngles event) {
+        Minecraft minecraft = Minecraft.getInstance();
+        if (minecraft.level == null || minecraft.player == null) return;
+        if (minecraft.level.dimension() != MachineDefence.MACHINE_DEFENCE_LEVEL) return;
+        if (minecraft.player.getXRot() < 85.0F) return;
+        event.setPitch(90.0F);
+        event.setRoll(0.0F);
+    }
+}
