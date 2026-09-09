e8715ec0a5b339a1649f69c2a104041a7131ad00
Desm00nt <deniska.kartuzov@gmail.com>
2026-09-09 18:07:50 +0300

    Add Machine Defence mod entry point

---
+++ src/main/java/com/desm00nt/machinedefence/MachineDefence.java
@@ -0,0 +1,12 @@
+package com.desm00nt.machinedefence;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MachineDefence.MOD_ID)
+public final class MachineDefence {
+    public static final String MOD_ID = "machinedefence";
+
+    public MachineDefence() {
+        // Registries and gameplay systems are added incrementally from this entry point.
+    }
+}
